f907f39d2c00a668f1d114cf13d329920c292af5
karakuscem <karakuscemenes@gmail.com>
2023-05-27 17:35:12 +0300

    init done

--- philo.c
+++ philo.c
@@ -7,6 +7,20 @@
 // Create a start simulation function
 // Create a free simulation function
 
+int init_simulation(t_data *simulation, int argc, char **argv)
+{
+    simulation->number_of_philosophers = atoi(argv[1]);
+    simulation->time_to_die = atoi(argv[2]);
+    simulation->time_to_eat = atoi(argv[3]);
+    simulation->time_to_sleep = atoi(argv[4]);
+    if (argc == 6)
+        simulation->number_of_times_each_philosopher_must_eat = atoi(argv[5]);
+    else
+        simulation->number_of_times_each_philosopher_must_eat = -1;
+    simulation->someone_died = false;
+    return (0);
+}
+
 int main(int argc, char **argv)
 {
     t_data *simulation;
--- philo.h
+++ philo.h
@@ -7,6 +7,16 @@
 #include <sys/time.h>
 #include <unistd.h>
 
+typedef struct s_data
+{
+    int number_of_philosophers;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int number_of_times_each_philosopher_must_eat;
+    bool someone_died;
+} t_data;
 
+int init_simulation(t_data *simulation, int argc, char **argv);
 
 #endif
